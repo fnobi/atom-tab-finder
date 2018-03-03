149d1213c3c3b23a7434daf0cccde8bebda6f923
nobii <fujisawa-shin@kayac.com>
2018-03-03 16:30:12 +0900

    first commit

---
+++ spec/atom-insert-image-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomInsertImageView from '../lib/atom-insert-image-view';
+
+describe('AtomInsertImageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
